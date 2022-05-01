a7d4f3615fbf8ed9fc6802b1491c60c80e833087
Romanojp <jp_romana@outlook.com>
2022-05-01 00:47:10 -0300

    aula 6.12 Alterando a estrategia fecth para Lazy

--- src/main/java/com/algaworks/algafood/domain/model/Grupo.java
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,12 +13,15 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Grupo {
 
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -27,6 +33,6 @@ public class Grupo {
 	@JoinTable(name = "grupo_permissao" , 
 	joinColumns = @JoinColumn(name = "grupo_id" ),
 	inverseJoinColumns = @JoinColumn(name = "permissao_id"))
-	private Permissao permissao;
+	private List<Permissao> permissoes = new ArrayList<>();
 	
 }
