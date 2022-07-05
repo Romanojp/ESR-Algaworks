d156df290edeeb3ae0df904bba96d6821915cada
Romanojp <jp_romana@outlook.com>
2022-07-05 17:56:07 -0300

    10.14. Limpando e populando o banco de dados de teste

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -6,30 +6,61 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
 	
 	@Before
 	public void setup() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port= port;
 		RestAssured.basePath = "/cozinhas";
+	
+		databaseCleaner.clearTables();
+		prepararDados();
+	
+		
 		
 	};
 	
  
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+		cozinhaRepository.save(cozinha1);
+		
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Americana");
+		cozinhaRepository.save(cozinha2);
+	}
+
+
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
 		
@@ -51,7 +82,19 @@ public class CadastroCozinhaIT {
 		.when()
 			.get()
 		.then()
-			.body("nome", Matchers.hasSize(4))
-			.body("nome", Matchers.hasItems("Indiana","Tailandesa"));
+			.body("nome", Matchers.hasSize(2));
+			
+	}
+	
+	@Test
+	public void deveRetornarStatus201_quandoCadastrarCozinha() {
+		given()
+			.body("{ \"nome\":\"Chinesa\"}")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 }
