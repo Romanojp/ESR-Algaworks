a23bc76405130e42c4347e760451b1e77d8904a8
Romanojp <jp_romana@outlook.com>
2022-07-07 17:35:27 -0300

    11.14. Adicionando e usando o ModelMapper

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.model.mixin.CidadeMixin;
 import com.algaworks.algafood.domain.model.mixin.CozinhaMixin;
-import com.algaworks.algafood.domain.model.mixin.RestauranteMixin;
 import com.algaworks.algafood.domain.model.mixin.UsuarioMixin;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -17,7 +15,7 @@ public class JacksonMixinModule extends SimpleModule{
 	private static final long serialVersionUID = 1L;
 	
 	public JacksonMixinModule() {
-		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+		
 		setMixInAnnotation(Cidade.class, CidadeMixin.class);
 		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
 		setMixInAnnotation(Usuario.class, UsuarioMixin.class);
---
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelmapperConfig.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.core.modelmapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelmapperConfig {
+	
+	@Bean
+	public ModelMapper modelmapper() {
+		return new ModelMapper();
+		
+	}
+
+}
